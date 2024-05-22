15480cc0d7ae4d81d4261149339af2757a9f90df
seotaeyang <seotaeyang@naver.com>
2024-05-22 22:56:14 +0900

    [AGR-60] 행동 칸 팝업창 구현

--- src/pages/mainboard.js
+++ src/pages/mainboard.js
@@ -1,98 +1,205 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from '../styles/MainBoard.module.css';
 
+const imagesInfo = [
+    {
+        path: "/images/bush.png",
+        x: 5,
+        y: 20,
+        width: 180,
+        height: 110,
+        popupBg: "/images/popup/popup_bush.png"
+    },
+    {
+        path: "/images/boscage.png",
+        x: 5,
+        y: 150,
+        width: 180,
+        height: 110,
+        popupBg: "/images/popup/popup_boscage.png"
+    },
+    {
+        path: "/images/resource_market.png",
+        x: 5,
+        y: 283,
+        width: 180,
+        height: 80,
+        popupBg: "/images/popup/popup_resource_market.png"
+    },
+    {
+        path: "/images/clay_mine.png",
+        x: 5,
+        y: 373,
+        width: 180,
+        height: 80,
+        popupBg: "/images/popup/popup_clay_mine.png"
+    },
+    {
+        path: "/images/traveling_theater.png",
+        x: 5,
+        y: 463,
+        width: 180,
+        height: 80,
+        popupBg: "/images/popup/popup_traveling_theater.png"
+    },
+    {
+        path: "/images/tutoring.png",
+        x: 5,
+        y: 566,
+        width: 180,
+        height: 155,
+        popupBg: "/images/popup/popup_tutoring.png"
+    },
+    {
+        path: "/images/farmland.png",
+        x: 5,
+        y: 731,
+        width: 180,
+        height: 80,
+        popupBg: "/images/popup/popup_farmland.png"
+    },
+    {
+        path: "/images/farm_expansion.png",
+        x: 203,
+        y: 15,
+        width: 180,
+        height: 260,
+        popupBg: "/images/popup/popup_farm_expansion.png"
+    },
+    {
+        path: "/images/dirt_mine.png",
+        x: 203,
+        y: 283,
+        width: 180,
+        height: 80,
+        popupBg: "/images/popup/popup_dirt_mine.png"
+    },
+    {
+        path: "/images/delivery_seller.png",
+        x: 203,
+        y: 373,
+        width: 180,
+        height: 80,
+        popupBg: "/images/popup/popup_delivery_seller.png"
+    },
+    {
+        path: "/images/grain_seed.png",
+        x: 203,
+        y: 463,
+        width: 180,
+        height: 80,
+        popupBg: "/images/popup/popup_grain_seed.png"
+    },
+    {
+        path: "/images/meeting_place.png",
+        x: 203,
+        y: 566,
+        width: 180,
+        height: 110,
+        popupBg: "/images/popup/popup_meeting_place.png"
+    },
+    {
+        path: "/images/forest.png",
+        x: 203,
+        y: 696,
+        width: 180,
+        height: 110,
+        popupBg: "/images/popup/popup_forest.png"
+    },
+    {
+        path: "/images/reed_field.png",
+        x: 401,
+        y: 10,
+        width: 180,
+        height: 60,
+        popupBg: "/images/popup/popup_reed_field.png"
+    },
+    {
+        path: "/images/fishing.png",
+        x: 401,
+        y: 110,
+        width: 180,
+        height: 60,
+        popupBg: "/images/popup/popup_fishing.png"
+    },
+    {
+        path: "/images/main_facility.png",
+        x: 401,
+        y: 190,
+        width: 180,
+        height: 80,
+        popupBg: "/images/popup/popup_main_facility.png"
+    }
+];
+
 const MainBoard = () => {
-    const handleBush1Click = () => {
-        alert('bush1 클릭');
-    }
-    const handleBush2Click = () => {
-        alert('bush2 클릭');
-    }
-    const handleResourceMarketClick = () => {
-        alert('Resource Market 클릭');
-    }
-    const handleClayMineClick = () => {
-        alert('Clay Mine 클릭');
-    }
-    const handleTeachingClick = () => {
-        alert('Teaching 클릭');
-    }
-    const handleTroupeClick = () => {
-        alert('Troupe 클릭');
-    }
-    const handleFarmLandClick = () => {
-        alert('Farm Land 클릭');
-    }
-    const handleExpandFarmClick = () => {
-        alert('Expand Farm 클릭');
-    }
-    const handleMeetingRoomClick = () => {
-        alert('Meeting Room 클릭');
-    }
-    const handleGrainSeedClick = () => {
-        alert('Grain Seed 클릭');
-    }
-    const handleDayLaborerClick = () => {
-        alert('Day Laborer 클릭');
-    }
-    const handleForestClick = () => {
-        alert('Forest 클릭');
-    }
-    const handleDirtMineClick = () => {
-        alert('Dirt Mine 클릭');
-    }
-    const handleReedFieldClick = () => {
-        alert('Reed Field 클릭');
-    }
-    const handleFishingClick = () => {
-        alert('Fishing 클릭');
-    }
-    const handleMainFacilityClick = () => {
-        alert('Main Facility 클릭');
-    }
+    const [popupVisible, setPopupVisible] = useState(false);
+    const [selectedImageInfo, setSelectedImageInfo] = useState(null);
+    const [modalSize, setModalSize] = useState({ width: '60vw', height: '60vh' });
 
+    const handleImageClick = (imageInfo) => {
+        setSelectedImageInfo(imageInfo);
+        setPopupVisible(true);
+    };
 
+    const handleClosePopup = () => {
+        setSelectedImageInfo(null);
+        setPopupVisible(false);
+    };
+
+    useEffect(() => {
+        if (selectedImageInfo) {
+            const img = new Image();
+            img.onload = () => {
+                const aspectRatio = img.width / img.height;
+                setModalSize({
+                    width: `${60 * aspectRatio}vh`,
+                    height: '60vh'
+                });
+            };
+            img.src = selectedImageInfo.popupBg;
+        }
+    }, [selectedImageInfo]);
+
+    const handleChoose = () => {
+        alert("구현예정");
+        // 행동 칸의 '선택하기' 버튼을 눌렀을 때 발생하는 로직 필요
+    }
 
     return (
         <div className={styles.fullScreen}>
             <div className={styles.backgroundContainer}>
-                <svg viewBox="0 0 1728 1117" preserveAspectRatio="xMidYMid meet" style={{ width: '100%', height: '100%' }}>
-                    {/* 덤불 */}
-                    <image xlinkHref="/" x="5" y="20" width="180" height="110" onClick={handleBush1Click} />
-                     {/* 수풀 */}
-                    <image xlinkHref="/" x="5" y="150" width="180" height="110" onClick={handleBush2Click} fill="transparent" />
-                    {/* 자원시장 */}
-                    <image xlinkHref="/" x="5" y="283" width="180" height="80" onClick={handleResourceMarketClick} fill="transparent" />
-                    {/* 점토 채굴장 */}
-                    <image xlinkHref="/" x="5" y="373" width="180" height="80" onClick={handleClayMineClick} fill="transparent" />
-                    {/* 유랑극단 */}
-                    <image xlinkHref="/" x="5" y="463" width="180" height="80" onClick={handleTroupeClick} fill="transparent" />
-                    {/* 교습 */}
-                    <image xlinkHref="/" x="5" y="566" width="180" height="155" onClick={handleTeachingClick} fill="transparent" />
-                    {/* 농지 */}
-                    <image xlinkHref="/" x="5" y="731" width="180" height="80" onClick={handleFarmLandClick} fill="transparent" />
-                    {/* 농장 확장 */}
-                    <image xlinkHref="/" x="203" y="15" width="180" height="260" onClick={handleExpandFarmClick} fill="transparent" />
-                    {/* 흙 채굴장 */}
-                    <image xlinkHref="/" x="203" y="283" width="180" height="80" onClick={handleDirtMineClick} fill="transparent" />
-                    {/* 날품팔이 */}
-                    <image xlinkHref="/" x="203" y="373" width="180" height="80" onClick={handleDayLaborerClick} fill="transparent" />
-                    {/* 곡식 종자 */}
-                    <image xlinkHref="/" x="203" y="463" width="180" height="80" onClick={handleGrainSeedClick} fill="transparent" />
-                    {/* 회합 장소 */}
-                    <image xlinkHref="/" x="203" y="566" width="180" height="110" onClick={handleMeetingRoomClick} fill="transparent" />
-                    {/* 숲 */}
-                    <image xlinkHref="/" x="203" y="696" width="180" height="110" onClick={handleForestClick} fill="transparent" />
-                    {/* 갈대밭 */}
-                    <image xlinkHref="/" x="401" y="10" width="180" height="60" onClick={handleReedFieldClick} fill="transparent" />
-                    {/* 낚시 */}
-                    <image xlinkHref="/" x="401" y="110" width="180" height="60" onClick={handleFishingClick} fill="transparent" />
-                    {/* 주요설비 */}
-                    <image xlinkHref="/" x="401" y="190" width="180" height="80" onClick={handleMainFacilityClick} fill="transparent" />
+                <svg viewBox="0 0 1728 1117" preserveAspectRatio="none" style={{ width: '100%', height: '100%' }}>
+                    {imagesInfo.map((imageInfo, index) => (
+                        <image
+                            key={index}
+                            xlinkHref={imageInfo.path}
+                            x={imageInfo.x}
+                            y={imageInfo.y}
+                            width={imageInfo.width}
+                            height={imageInfo.height}
+                            onClick={() => handleImageClick(imageInfo)}
+                        />
+                    ))}
                 </svg>
             </div>
+
+            {popupVisible && (
+                <div className={styles.overlay}>
+                    <div className={styles.modal} style={{ width: modalSize.width, height: modalSize.height }}>
+                        <div className={styles.modalBackground}>
+                            <div className={styles.backgroundImage} style={{ backgroundImage: `url(${selectedImageInfo.popupBg})` }}>
+                                <div className={styles.modalContent}>
+                                    <span className={styles.closeButton} onClick={handleClosePopup} />
+                                    <button className={styles.chooseButton} onClick={handleChoose}></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
-};
+}
 
 export default MainBoard;
