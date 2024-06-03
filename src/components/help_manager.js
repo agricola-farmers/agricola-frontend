9e87771b4df5bee68a87c32f983e0e311526d98b
yu00hun <yu00hun@naver.com>
2024-06-03 23:38:25 +0900

    [AGR-67] 도움말, 점수 수정

--- src/components/help_manager.js
+++ src/components/help_manager.js
@@ -1,23 +1,13 @@
+import Image from 'next/image';
 import React, { useState } from 'react';
+import styles from '../styles/HelpManager.module.css';
 
-const HelpModal = ({ onClose, images }) => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  const handleNext = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
-  const handlePrev = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-  };
-
+const HelpModal = ({ onClose }) => {
   return (
-    <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'white', padding: '20px', zIndex: 1000, borderRadius: '10px' }}>
-      <button onClick={onClose} style={{ position: 'absolute', right: '10px', top: '10px' }}>닫기</button>
-      <img src={images[currentImageIndex]} alt={`도움말 이미지 ${currentImageIndex + 1}`} style={{ width: '100%', height: 'auto' }} />
-      <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
-        <button onClick={handlePrev}>이전</button>
-        <button onClick={handleNext}>다음</button>
+    <div className={styles.background} onClick={onClose}>
+      <div className={styles.container}>
+        <Image src="/images/help_image.png" width={600} height={808} />
+        <div className={styles.close} onClick={onClose} />
       </div>
     </div>
   );
@@ -28,16 +18,10 @@ export const HelpManager = () => {
 
   const toggleHelp = () => setShowHelp(!showHelp);
 
-  const images = [
-    '/images/help_image1.png',
-    '/images/help_image2.png',
-    '/images/help_image3.png',
-    // 필요한 만큼 이미지 추가
-  ];
-
   return {
     showHelp,
     toggleHelp,
-    HelpModalComponent: () => showHelp && <HelpModal onClose={() => setShowHelp(false)} images={images} />
+    HelpModalComponent: () =>
+      showHelp && <HelpModal onClose={() => setShowHelp(false)} />,
   };
 };
